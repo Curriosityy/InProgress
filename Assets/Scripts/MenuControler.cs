6fc0f8ba7d0904875881b0c2feb23099014e6c8a
Curriosityy <patryk9595@gmail.com>
2018-11-25 21:57:11 +0100

    -set menu controler -created folders

---
+++ Assets/Scripts/MenuControler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControler : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelector;
+    public GameObject options;
+    private bool b_menu;
+    private bool b_levelSelector;
+    private bool b_options;
+
+    private void Start()
+    {
+        b_levelSelector = false;
+        b_menu = true;
+        b_options = false;
+    }
+
+    public void GoToLevelSelector()
+    {
+        b_levelSelector = true;
+        b_menu = false;
+        b_options = false;
+        SetScenes();
+    }
+
+    public void GoToOptions()
+    {
+        b_levelSelector = false;
+        b_menu = false;
+        b_options = true;
+        SetScenes();
+    }
+
+    public void GoToMenu()
+    {
+        b_levelSelector = false;
+        b_menu = true;
+        b_options = false;
+        SetScenes();
+    }
+
+    private void SetScenes()
+    {
+        mainMenu.SetActive(b_menu);
+        options.SetActive(b_options);
+        levelSelector.SetActive(b_levelSelector);
+    }
+
+    public void ExitApplication()
+    {
+    }
+}
