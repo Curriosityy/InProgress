d03d7e9b83a92d9f5a16ae6eacb5ea112a5b8dd7
Curriosityy <patryk9595@gmail.com>
2018-12-02 18:05:41 +0100

    added audio controler to follow camera. added 1 song

---
+++ Assets/AudioControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControler : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        cam = Camera.main;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        transform.position = cam.transform.position;
+    }
+}
